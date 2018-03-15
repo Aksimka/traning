c2b3b78172a14d0964b5b2ecc7944d5ec9eb44d6
Aksimka <36276085+Aksimka@users.noreply.github.com>
2018-03-16 00:06:41 +0300

    imposible

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>test</title>
+</head>
+<style>
+	body{
+		text-align: center;
+	}
+
+	.container{
+		width: 30%;
+		border: 1px solid lightgrey;
+		margin: 10px;
+		text-align: center;
+	}
+	.main{
+		display: flex;
+		-webkit-justify-content: center;
+		        justify-content: center;
+	}
+	h2{
+		text-transform: uppercase;
+		text-decoration: underline;
+		
+	}
+</style>
+<body>
+	<h1>It is my first php page!</h1>
+	<div class="main">
+
+		<?php
+			
+		?>
+	</div>
+</body>
+</html>
